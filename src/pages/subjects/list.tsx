06911e142844c43b6798f6ee78577750e73c8e25
Zarina Kylie <zarinakylielagera@gmail.com>
2026-07-01 04:28:15 +0800

    implement data provider

--- src/pages/subjects/list.tsx
+++ src/pages/subjects/list.tsx
@@ -22,7 +22,7 @@ const SubjectListPage = () => {
         () => [
             {
                 id: "code",
-                accessorKey: "code",
+                accessorKey: "courseCode",
                 size: 100,
                 header: () => <p className="column-title ml-2">Code</p>,
                 cell: ({ getValue }) => <Badge>{getValue<string>()}</Badge>,
@@ -39,7 +39,7 @@ const SubjectListPage = () => {
             },
             {
                 id: "department",
-                accessorKey: "department.name",
+                accessorKey: "department",
                 size: 150,
                 header: () => <p className="column-title">Department</p>,
                 cell: ({ getValue }) => (
--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,16 +1,16 @@
 
 import {BaseRecord, DataProvider, GetListParams, GetListResponse} from "@refinedev/core";
+import { MOCK_SUBJECTS } from "../constants/mock-data";
 
 export const dataProvider: DataProvider = {
-  getList: async <TData extends BaseRecord = BaseRecord>({resource,}:
-   GetListParams): Promise<GetListResponse<TData>> => {
+  getList: async <TData extends BaseRecord = BaseRecord>({resource,}: GetListParams): Promise<GetListResponse<TData>> => {
     if (resource !== "subjects") {
       return { data: [] as TData[], total: 0 };
     }
 
     return {
-      data: [],
-      total: 0,
+      data: MOCK_SUBJECTS as unknown as TData[],
+      total: MOCK_SUBJECTS.length,
     };
   },
 
